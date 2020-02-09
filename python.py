3013e6ffe4680b58fdd061191417c6146bdcd58d
EC2 Default User <ec2-user@ip-172-31-29-201.eu-west-1.compute.internal>
2020-02-09 15:48:36 +0000

    first commit

---
+++ python.py
@@ -0,0 +1,13 @@
+width = float(input("enter the width in Meters"))
+    
+length = float(input("enter length in Meters"))
+
+depth = float(input("enter depth in Meters"))
+
+result = depth * length * width * 2.3
+
+def calculate():
+   print(result)
+   
+   
+calculate()
